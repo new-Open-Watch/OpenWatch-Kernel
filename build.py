288077f137a44307e45db743873c633f0d42b7fe
Michasssada <michaslate@gmail.com>
2026-06-12 20:59:33 +0200

    updated build.py

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ argumentParser = argparse.ArgumentParser()
 argumentParser.add_argument("-p","--pal",action="store_true", help="sets OpenWatch port")
 arguments = argumentParser.parse_args()
 def updateOWGL():
+    print("updating owgl")
     if not os.path.isdir("components/owgl"):
         subprocess.run(["git", "submodule", "add" ,"https://github.com/new-Open-Watch/OpenWatch-general-library.git" ,"components/owgl"])
         subprocess.run(["git","submodule", "update", " --init", "--recursive"])
